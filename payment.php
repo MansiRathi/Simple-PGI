8732a6edc8a807d1ced1b3672dfae2f1c46183e0
MansiRathi <mansi.rathi62@gmail.com>
2020-06-28 14:34:19 +0530

    Simple payement integration website!

---
+++ payment.php
@@ -0,0 +1,38 @@
+<?php
+
+$Name=$_POST['name'];
+$Email=$_POST['email'];
+$Amount=$_POST['amount'];
+//$Phone=$_POST['phone'];
+$purpose='Donation';
+
+
+include 'instamojo/Instamojo.php';
+
+$api = new Instamojo\Instamojo('test_6620ec1c9ff2cd530b90d1ec36d', 'test_69417cae46934ed9d74d90f1d43', 'https://test.instamojo.com/api/1.1/');
+
+
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $purpose,
+        "amount" => $Amount,
+        "send_email" => true,
+        "email" => $Email,
+        "buyer_name" =>$Name,
+       // "phone"=>$Phone,
+        "send_sms" => false,
+        "allow_repeated_payments" =>false,
+        "redirect_url" => "https://payementgatewayintegration.000webhostapp.com/thankyou.php"
+        ));
+    //print_r($response);
+    $pay_url=$response['longurl'];
+    header("location: $pay_url");
+	}
+	catch (Exception $e) {
+	    print('Error: ' . $e->getMessage());
+	}
+
+
+
+?>
+
